e7cfc9868caecdfe88cba7efd7c9a8d6d16a78cd
ngthduy <nguyenduy170898@gamil.com>
2020-03-15 15:33:46 +0700

    change color cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,7 +32,8 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url }
-        format.js
+        # truyền biến @current_item
+        format.js   { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
